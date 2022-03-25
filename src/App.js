f0bb60f01a50aa2dec3f38ab72315ffb4e6d2e5a
danacardozo <dana.n.cardozo@gmail.com>
2022-03-25 01:48:20 -0300

    maquetado de navbar, product y products

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
+import NavBar from "./components/NavBar"
 import Product from "./components/Product";
 import './App.css';
+import Products from "./components/Products";
 
 function App() {
   return (
     <div className="App"> 
-    <h1>ECOMERCE</h1>
-    <Product />
+    <NavBar/> 
+    <Products/>
+   {/* <Product />*/}
     </div>
   );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -6,19 +6,19 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
+import Collapse from '@material-ui/core/Collapse'; 
 import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Typography from '@material-ui/core/Typography'; 
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'; 
+import  {AddShoppingCart} from '@material-ui/icons'
+import accounting from "accounting";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
+  },
+  action: {
+    marginTop: "1rem",
   },
   media: {
     height: 0,
@@ -33,10 +33,7 @@ const useStyles = makeStyles((theme) => ({
   },
   expandOpen: {
     transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
+  }, 
 }));
 
 export default function Product() {
@@ -56,10 +53,10 @@ export default function Product() {
             variant="h5"
             color="textSecondary"
             >
-            {50}
+            {accounting.formatMoney(1000)}
             </Typography>
         }
-        title="album"
+        title="i brought you bullets, you brought me your love"
         subheader="in stock"
       />
       <CardMedia
@@ -69,17 +66,19 @@ export default function Product() {
       />
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
+         MCR ALBUM DEBUT
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
+        <IconButton aria-label="add to CART"  >
+    <AddShoppingCart fontSize="large"/>
         </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
+        
+        {Array(4)
+        .fill()
+        .map((_, i) => (
+          <p>&#11088;</p>
+        ))}
         <IconButton
           className={clsx(classes.expand, {
             [classes.expandOpen]: expanded,
@@ -93,29 +92,7 @@ export default function Product() {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
+          <Typography paragraph>descripcion</Typography>
         </CardContent>
       </Collapse>
     </Card>
