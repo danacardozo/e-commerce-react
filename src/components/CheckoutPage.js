de5e78250df50f1d118ab13303f4cc2fc88790f9
danacardozo <dana.n.cardozo@gmail.com>
2022-03-25 21:40:49 -0300

    basket

--- src/components/CheckoutPage.js
+++ src/components/CheckoutPage.js
@@ -3,9 +3,9 @@ import {makeStyles} from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import {Typography} from "@material-ui/core/"; 
 import CheckoutCard from "./CheckoutCard"; 
-import products from "../product-data";
 import Product from "./Product"
 import Total from "./Total";
+import {useStateValue} from "../StateProvider";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -16,12 +16,13 @@ const useStyles = makeStyles((theme) => ({
 
 const CheckoutPage = () => {
     const classes = useStyles(); 
+    const [{basket}, dispatch] = useStateValue();
 
 
 function FormRow() {
     return(
         <React.Fragment>
-            {products?.map((item) => (
+            {basket?.map((item) => (
                 <Grid item xs={12} sm={8} md={6} lg={4}>
                     <CheckoutCard key={item.id} product={item}/>
                 </Grid>
